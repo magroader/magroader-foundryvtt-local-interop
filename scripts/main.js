e59f0ed29d2df55f72b210dec2e6b295cefb86be
Colin C <colincampbell@ea.com>
2022-10-18 11:53:06 -0400

    Successfully posting a request, tested with postman

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,17 @@ Hooks.on("updateCombat", function(combat) {
   var actor = combatant.actor;
   if (actor == undefined)
     return;
-  console.log("magroader current combat actor: " + actor.name);
+  //console.log("magroader current combat actor: " + actor.name);
+  post("updateCombat", {hook: "combatUpdate", "currentCombatantName": actor.name});
 });
 
 Hooks.on("deleteCombat", function(params) {
-  console.log("magroader combat ended");
+  //console.log("magroader combat ended");
+  post("endCombat", {});
 });
+
+function post(path, data) {
+  //var url = "https://14f1b025-4626-454a-921c-b04882be24b0.mock.pstmn.io/"
+  var url = "http://localhost:31832/"
+  return fetch(url + path, {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+}
